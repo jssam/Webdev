c589199d6b8262554ff8e5963a1c26ba9cea4da3
jssam <jains3099@gmail.com>
2021-05-02 11:07:12 +0530

    excel clone

---
+++ browser/excel_clone/script.js
@@ -0,0 +1,54 @@
+let addbtnContainer = document.querySelector(".add-sheet_container");
+let sheetList = document.querySelector(".sheets-list");
+let firstSheet = document.querySelector(".sheet");
+firstSheet.addEventListener("click",handleActiveSheet);
+addbtnContainer.addEventListener("click",function(){
+    let sheetsArr = document.querySelectorAll(".sheet");
+    let lastSheetElem = sheetsArr[ sheetsArr.length - 1];
+    let idx = lastSheetElem.getAttribute("sheetIdx");
+    idx = Number(idx);
+    let NewSheet = document.createElement("div");
+    NewSheet.setAttribute("class","sheet");
+    NewSheet.setAttribute("sheetIdx",idx+1);
+    NewSheet.innerText = `sheet ${idx+1}`;
+    sheetList.appendChild(NewSheet);
+    NewSheet.addEventListener("click",handleActiveSheet);
+})
+
+function handleActiveSheet(e){
+let MySheet = e.currentTarget;
+let sheetsArr = document.querySelectorAll(".sheet");
+sheetsArr.forEach(function(sheet){
+       sheet.classList.remove("active-sheet");
+})
+if(!MySheet.classList[1]){
+    MySheet.classList.add("active-sheet");
+}
+
+}
+
+/////sheet maker
+let topRow = document.querySelector(".top-row");
+        let str = "";
+        for (let i = 0; i < 26; i++) {
+            str += `<div class='col'>${String.fromCharCode(65 + i)}</div>`;
+        }
+        topRow.innerHTML = str;
+        let leftCol = document.querySelector(".left-col");
+        str = ""
+        for (let i = 0; i < 100; i++) {
+            str += `<div class='left-col_box'>${i + 1}</div>`
+        }
+        leftCol.innerHTML = str;
+
+        // 2d array
+        let grid = document.querySelector(".grid");
+        str = "";
+        for (let i = 0; i < 100; i++) {
+            str += `<div class="row">`
+            for (let j = 0; j < 26; j++) {
+                str += `<div class='col' contentEditable="true">${String.fromCharCode(65 + j)}${i + 1}</div>`
+            }
+            str += "</div>";
+        }
+        grid.innerHTML = str;
--- browser/todo full/script.js
+++ browser/todo full/script.js
@@ -1,5 +1,6 @@
 let colorbtn = document.querySelectorAll(".filter");
 let maincontainer1 = document.querySelector(".maincontainer");
+let deletec=false;
 for(let i =0;i<colorbtn.length;i++){
     colorbtn[i].addEventListener("click",function(e){
         let color = colorbtn[i].querySelectorAll(".filter_color");
@@ -14,14 +15,18 @@ for(let i =0;i<colorbtn.length;i++){
         // }
     })
 }
-
+let cross = document.querySelectorAll(".icon");
+cross[1].addEventListener("click",changedel);
 let newbox = document.querySelector(".icon");
 let body = document.body;
 newbox.addEventListener("click",function(e){
     console.log("das");
+    let box = document.querySelector(".hell");
     let modal_container = document.createElement("div");
+    if(box==null){
     modal_container.setAttribute("class", "maincontainer");
-    modal_container.innerHTML =`  <div class="writeparent" contenteditable="true"></div>
+    modal_container.innerHTML =`  <textarea class="writeparent" placeholder="Enter Your text"></textarea>
+    <div class="hell"></div>
     <div class="colorselector">
         <div class="filtercolor pink"></div>
         <div class="filtercolor green"></div>
@@ -29,15 +34,63 @@ newbox.addEventListener("click",function(e){
         <div class="filtercolor black"></div>
     </div>`
     body.appendChild(modal_container);
-    
-let colorbtan = document.querySelectorAll(".filtercolor");
+}
+let textarea = modal_container.querySelector(".writeparent");
+textarea.value = "";
 
+let colorbtan = document.querySelectorAll(".filtercolor");
+let cclor = "black";
 for(let i =0;i<colorbtan.length;i++){
     colorbtan[i].addEventListener("click",function(e){
-    colorbtan[i].style.border = "3px solid black" ;
+    ////purane se remove kar dete hai
+        colorbtan.forEach((filter)=>
+    {filter.classList.remove("border");})
+    colorbtan[i].classList.add("border");
+    ///color set kar dega
+    cclor = colorbtan[i].classList[1];
+    console.log(cclor);
     })}
+    let textArea = document.querySelector(".writeparent");
+    textArea.addEventListener("keydown",function(e){
+        if(e.key == "Enter"&& textarea.value != ""){
+            console.log("task",textarea.value,cclor);
+            modal_container.remove();
+            createtask(cclor,textArea.value);
+        }
+    })
 })
+function createtask(color,task){
+    let taskContainer = document.createElement("div");
 
+    let uifn = new ShortUniqueId();
+    let uid = uifn();
+    taskContainer.setAttribute("class", "task_container");
+    taskContainer.innerHTML = `<div class="task_filter ${color}"></div>
+    <div class="task_desc_container">
+        <h3 class="uid">#${uid}</h3>
+        <div class="task_desc" contenteditable="true" >${task}</div>
+    </div>
+</div >`;
+    maincontainer1.appendChild(taskContainer);
+    let taskFilter = taskContainer.querySelector(".task_filter");
+    taskFilter.addEventListener("click", changeColor);
+    taskContainer.addEventListener("click", deleteTask);
+}
+
+function changeColor(e) {
+    //  add event listener 
+    // console.log(e.currentTarget);
+    // /event occur 
+    // console.log(e.target);
+    let taskFilter = e.currentTarget;
+    let colors = ["pink", "blue", "green", "black"];
+    let cColor = taskFilter.classList[1];
+    let idx = colors.indexOf(cColor);
+    let newColorIdx = (idx + 1) % 4;
+    ///purane vala htana padega taki stack overflow nahi aye
+    taskFilter.classList.remove(cColor);
+    taskFilter.classList.add(colors[newColorIdx]);
+}
 
         // colorbtn[i].style.backgroundColor = "rgb(36, 34, 34)";
         // for(let a =0;a<colorbtn.length;a++){
@@ -46,3 +99,13 @@ for(let i =0;i<colorbtan.length;i++){
         //     }
         // }
 
+function changedel(e){
+    deletec = !deletec;
+    console.log(deletec);
+}
+function deleteTask(e){
+if(deletec){
+    let taskContainer = e.currentTarget;
+    taskContainer.remove();
+}
+}
